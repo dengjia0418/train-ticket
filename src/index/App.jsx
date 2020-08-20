968f63db3f02fcf1ffa8dac7809a4a605b0c3ba9
missDJ <18279167511@163.com>
2020-08-20 22:05:24 +0800

    index页面组件架构

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,8 +1,23 @@
+import React from 'react'
 import { connect } from 'react-redux';
 import './App.css';
 
-function App(props) {
+import Header from '../common/Header.jsx';
+import DepartDate from './DepartDate.jsx';
+import HighSpeed from './HighSpeed.jsx';
+import Journey from './Journey.jsx';
+import Submit from './Submit.jsx';
 
+function App(props) {
+    return (
+        <div>
+            <Header/>
+            <Journey/>
+            <DepartDate/>
+            <HighSpeed/>
+            <Submit/>
+        </div>
+    )
 }
 
 export default  connect (
